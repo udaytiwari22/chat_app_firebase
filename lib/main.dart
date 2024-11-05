6cfdced40f35e4066088c4ce3c3efb800ed38228
Uday Tiwari <udaytiwaricyef2001@gmail.com>
2024-11-05 09:04:40 +0530

    inital

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
-import 'package:chat_app/screens/auth.dart';
-import 'package:chat_app/screens/chat.dart';
-import 'package:chat_app/screens/splash.dart';
+import 'package:chat_app_firebase/screens/auth.dart';
+import 'package:chat_app_firebase/screens/chat.dart';
+import 'package:chat_app_firebase/screens/splash.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
@@ -10,7 +10,7 @@ void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
-  );
+  ); 
   runApp(const App());
 }
 
